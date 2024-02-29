b2da88e619500a4b7cd555e870bd279be1031e7c
l4vd <79866926+l4vd@users.noreply.github.com>
2024-02-29 22:23:15 +0100

    + to be tried tomorrow

--- Java/src/GraphReader.java
+++ Java/src/GraphReader.java
@@ -34,3 +34,43 @@ public class GraphReader {
         System.out.println("Edges: " + graph.edgeSet());
     }
 }
+
+
+import org.gephi.graph.api.*;
+        import org.gephi.io.importer.api.*;
+        import org.gephi.io.processor.plugin.DefaultProcessor;
+        import org.gephi.project.api.ProjectController;
+        import org.gephi.project.api.Workspace;
+        import org.openide.util.Lookup;
+
+        import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        // Initialize the Gephi toolkit
+        ProjectController pc = Lookup.getDefault().lookup(ProjectController.class);
+        pc.newProject();
+        Workspace workspace = pc.getCurrentWorkspace();
+
+        // Import file
+        ImportController importController = Lookup.getDefault().lookup(ImportController.class);
+        Container container;
+        try {
+            File file = new File("path/to/your/file.gexf");
+            container = importController.importFile(file);
+            container.getLoader().setEdgeDefault(EdgeDirectionDefault.DIRECTED);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return;
+        }
+
+        // Append imported data to GraphAPI
+        importController.process(container, new DefaultProcessor(), workspace);
+
+        // Get a graph model - it exists because we have a workspace
+        GraphModel graphModel = Lookup.getDefault().lookup(GraphController.class).getGraphModel();
+
+        System.out.println("Nodes: " + graphModel.getGraph().getNodeCount());
+        System.out.println("Edges: " + graphModel.getGraph().getEdgeCount());
+    }
+}
